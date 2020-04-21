435b2245ff0157363f16d0d9a481238eef8570bf
fork52 <mrunankmistry52@gmail.com>
2020-04-21 20:08:41 +0530

    parameter choosing success

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,25 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+min_error = 10000;
+values_list =  [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30 ];
 
-
-
-
-
+for i=1:8,
+    for j = 1:8,
+        C_new = values_list(i);
+        sigma_new = values_list(j);
+        displaying = [i,j];
+        disp(displaying)
+        myModel = svmTrain(X, y, C_new, @(x1, x2) gaussianKernel(x1, x2, sigma_new));
+        predictions = svmPredict(myModel, Xval);
+        current_error = mean(double(predictions ~= yval));
+        if current_error < min_error,
+            min_error = current_error;
+            C = C_new;
+            sigma = sigma_new;
+        end;
+    end;
+end;
 
 % =========================================================================
 
