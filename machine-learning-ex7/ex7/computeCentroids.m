d5035f7df7fa980bd5ccb4885b1865fe956ffb86
fork52 <mrunankmistry52@gmail.com>
2020-04-22 19:36:35 +0530

    Kmeans completed!

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -16,6 +16,9 @@ function centroids = computeCentroids(X, idx, K)
 % You need to return the following variables correctly.
 centroids = zeros(K, n);
 
+
+
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Go over every centroid and compute mean of all points that
 %               belong to it. Concretely, the row vector centroids(i, :)
